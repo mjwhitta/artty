a6cbe6bc2d932d038138f0eb76247355c6cfd743
Miles Whittaker <mjwhitta@gmail.com>
2018-02-24 16:35:24 -0500

    Forgot to add a couple new files

---
+++ lib/arTTY/error.rb
@@ -0,0 +1,4 @@
+class ArTTY::Error < RuntimeError
+end
+
+require "arTTY/error/art_not_found"
---
+++ lib/arTTY/error/art_not_found.rb
@@ -0,0 +1,6 @@
+class ArTTY::Error::ArtNotFound < ArTTY::Error
+    def initialize(art = nil)
+        super("Art not found: #{art}") if (art)
+        super("Art not found") if (art.nil?)
+    end
+end
