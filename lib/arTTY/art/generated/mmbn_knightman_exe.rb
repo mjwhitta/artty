c4c8c1e2f99327fd7241b6c3f81cf16cce3131b7
Miles Whittaker <mjwhitta@gmail.com>
2018-04-09 22:51:41 -0400

    Added more megaman battle network

---
+++ lib/arTTY/art/generated/mmbn_knightman_exe.rb
@@ -0,0 +1,129 @@
+# encoding: utf-8
+class ArTTY::Art::MmbnKnightmanEXE < ArTTY::Art
+    def color_map
+        return [
+            "                                                                               λ",
+            "                                555555                                       ",
+            "                               4777777755                                    ",
+            "                              455777777775                                   ",
+            "                              4557777777775                                  ",
+            "                              45544444557775                                 ",
+            "                              45544444444577                                 ",
+            "                              455444444444575                                ",
+            "                              455444444444477                                ",
+            "                              4554444444444575                               ",
+            "                              4554444444444475                               ",
+            "                    bb        4554444444444454                               ",
+            "                    dfdb    ac000ccaa44444444                                ",
+            "                    d33fdb a022000cccaa444                                   ",
+            "                    bf333fdc0000000cccca                                     ",
+            "                    bff8333fdc0000cccccaaaa                                  ",
+            "                    bdf88f3333fdbbaaaaaaacca                                 ",
+            "                     bfb8bf8f33ffa22000cccca                                 ",
+            "                     bdf88fb88f8ba2200ccccca                                 ",
+            "                      bdd8bf87f78a22001i1cca                                 ",
+            "                     bdbdb8bd8db8a2201ib81ca                                 ",
+            "                    bfdaabb8b8bd8a2201i881ca                                 ",
+            "                   bfaa0caab8a8bba22011b81cca                                ",
+            "                   ba22220caaaaaaa2200111cbbbbbbbb                           ",
+            "                   a002222222222222200ccccb333fffdbb                         ",
+            "                   c0000022222222222000cccb33333fffdb                        ",
+            "                  a00000000000000000000cccb333333fffdb                       ",
+            "                  188110000000000000000cccb3333333fffb                       ",
+            "                 a8418ii000000000000000cccbf333333fffdb                      ",
+            "                 c44i1ii00i000000000000ccaacdf33333ffdb                      ",
+            "                a08488ii1ii100000000000cac0000f3333fffb                      ",
+            "                c01481110iii00000000000ac02200d3333fffb                      ",
+            "               a000111100i1000000000000a0222200f333fffb                      ",
+            "               a001i000000000000000000ac0222200d333fffb                      ",
+            "               c0ii1000000000000000000ac0022000d333fffb                      ",
+            "             ba00iii100000000000000000acc000000bbbbbbbb                      ",
+            "            bda00i10000000000000000000accc0000ccccca                         ",
+            "           bddc001000000000000000000000acccccccccca                          ",
+            "          bddd0000000000000fd0000000000acccca888aca                          ",
+            "         bdddf00000000000003f0000000000caca888888a                           ",
+            "         bdf333d0000000000f3fd000000000cca8aaa888a                           ",
+            "        acdf33333ffdd0000033ff000000000ccc8aaaa888b                          ",
+            "       ac0bdf333333333ffff33ffd00000000ccc8aaaa888db                         ",
+            "       ac0bddff3333333333333fff0000000ccccdaaaa888ddb                        ",
+            "        acbddddddff333333333ff33333333fffffaaaaaaaadb                        ",
+            "       8aaadcdddddddddffff33f333333333fffffacc000cab                         ",
+            "      8aaaadc000dddddddddddfdfffffffffffffac000000ca                         ",
+            "     8baaa8a00000000ddddddffdddddddddddddda0000000ca                         ",
+            "    8bbaaa8a0000000000addfffdddddddddddddda0000000cca                        ",
+            "    8bbaa88a0000000000a20dffddc00000cccceca0000000cca                        ",
+            "    88aaa88a0000000000a200ffddc0000cccccggec2220000ccaa                      ",
+            "    8a8aaa8a0000000000a200dfdc0000ccccccehhgec0000000cca                     ",
+            "    8b8aaaac0000000000a2000fdc000ccccccceghhhgec00000cca     cccc            ",
+            "    8ba88aac000000000a20000dc000cccccccceeghe99999999aca  cceeghc            ",
+            "    8aaaa8ac000000000a200000000cccccccccec99999999999999ceegghhgc            ",
+            "     8888aacc00000000a2000000ccccccccccc999999999999999999ghhhgec            ",
+            "       888accc0000000a20000ccccccccccc9999999999999999999999ggeec            ",
+            "         8acccccc0000a20ccccccccceccc999999999999999999999999eeec            ",
+            "          acccccccccca2ccccccccccc6hgec99aaaa999999999ceegghc9eec            ",
+            "          acccccccccca2ccccccccccch66hgecbbbaaa99999cghhhh6hc99ec            ",
+            "           accccccccca2cccccccccccg6666hgccbbaaa9999ghhh66hgc999c            ",
+            "           accccccccca2cccccccccccgh6666hcdcbbaa9999gh666hggc9999            ",
+            "           acccccccaaa2ccccccccc9cgg6666h11dcbaaa999eh66hggec99999           ",
+            "            accccaaaacaacccccccc9cggh66he11dcbaaa999cg6hgggec99999           ",
+            "            accaaaacccccacccccc99cegghhecddcbbaaa9999eggggeec999999          ",
+            "          bbaaaaaccccccccaacccc999ceeccbbccbbaaa999999ceeeec9999999          ",
+            " bb     bbf333ffaaaccccccccacc99999999aabbbbaaaa99999999cc9999999999         ",
+            " b3fdddbbbb333fffaaaccccccceee999999999aaaaaaaa999999999999999999999         ",
+            " bf3333333bffffffccaaaccceggge99999999999aaaa99999999999999999999999         ",
+            "  b3333333bdfffffdcccaceghhhh9999999999999999999999999999999999999999c       ",
+            "  bdffffffdbffffffccc1acegghh9999999999999999999999999999999999999999ec      ",
+            "  bbffffffdbffffffcccc11ceegg9999999999999999999999999999999999999999eec     ",
+            "   bdffffffbdfffffdcccc11ceeg9999999999999999999999999999999999999999geec    ",
+            "   bbffffffbdfffffdccccccccee9999999999999999999999999999999999999999ggeec   ",
+            "   bbffffffbbffffffccccccccce9999999999999999999999999999999999999999hhggec  ",
+            "    aaaaaaaabffffffcccccccccc999999999999ceeec99999999999999999999999hhhhgec ",
+            "          88aaaaaaaaaaaaaaab399999999999eh666ge9999999999999999999999gggec   ",
+            "          8888888888888888b333999999999cgh666hge99999999999999999999ceec     ",
+            "           888888888888888bbf3999999999cgh66hgggc9999999999999999999cc       ",
+            "            8888888888bbbbf3bf999999999ceh6hggggc9999999999999999999         ",
+            "             888888888b333333bf99999999ceehggggec999999999999999999          ",
+            "               8888888bb333333b999999999eeegggee9999999999999999999          ",
+            "                  8888bbf3333fdb99999999ceeeeeec999999999999999999c          ",
+            "                     8bbbffffffb999999999ceeeec9999999999999999999c          ",
+            "                      bbbffffffdb99999999999999999999999999999999ec          ",
+            "                      aabbfffffdbc999999999999999999999999999999gec          ",
+            "                        aaaaaaaaacggec9999999999999999999999999eegc          ",
+            "                                 cg66he99999999999999999999999ccccc          ",
+            "                                 cg666ge999999999999999999999                ",
+            "                                 cg66hggc99999999999999ceeec                 ",
+            "                                 cg6hggec999999999999ce66gec                 ",
+            "                                 cghgecc  9999999999cggh66ec                 ",
+            "                                 checc       9999999cegg66gc                 ",
+            "                                 ccc                ccegh6gc                 ",
+            "                                                      ccehgc                 ",
+            "                                                        ccgc                 ",
+            "                                                          cc                 ",
+            "                                                                             ",
+        ]
+    end
+
+    def initialize
+        super
+        @name = "mmbn-knightman.EXE"
+        map_color("0", "color_060")
+        map_color("1", "color_102")
+        map_color("2", "color_103")
+        map_color("3", "color_139")
+        map_color("4", "color_160")
+        map_color("5", "color_166")
+        map_color("6", "color_188")
+        map_color("7", "color_209")
+        map_color("8", "color_236")
+        map_color("9", "color_237")
+        map_color("a", "color_238")
+        map_color("b", "color_239")
+        map_color("c", "color_240")
+        map_color("d", "color_242")
+        map_color("e", "color_243")
+        map_color("f", "color_245")
+        map_color("g", "color_246")
+        map_color("h", "color_249")
+        map_color("i", "color_251")
+    end
+end
