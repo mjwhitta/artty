16fd13f867dc41cf0a1b99f1306b66fc24cfbafd
Miles Whittaker <mjwhitta@gmail.com>
2018-04-09 22:08:57 -0400

    Added some more megaman battle network

---
+++ lib/arTTY/art/generated/mmbn_megaman_exe_colonel_soul.rb
@@ -0,0 +1,76 @@
+# encoding: utf-8
+class ArTTY::Art::MmbnMegamanEXEColonelSoul < ArTTY::Art
+    def color_map
+        return [
+            "                         2         λ",
+            "                         4c      ",
+            "                         5ac     ",
+            "                         5ac     ",
+            "                     999949ac    ",
+            "               9    99549499c    ",
+            "                99 9994c4299ac   ",
+            "             9   999994acca99c   ",
+            "              99 9999992acc99ac  ",
+            "               9999245929aca9ac  ",
+            "                9929045999ac99a  ",
+            "            aaa  92000247999a03  ",
+            "          aaddba9929092472a9990  ",
+            "         ad777db99a222095a42999  ",
+            "         a7d777da99ac039a552     ",
+            "        ad7dd77da99aac030aa9     ",
+            "        ad77d77da9a9a400330      ",
+            "         b7777da9aaa29929        ",
+            "         abddda9aaca492a         ",
+            "          aaaa99aaac42a      99  ",
+            "         aaa99299aaa259    99aa9 ",
+            "        aaa999429992409  aaa9ab99",
+            "        00009242a99209aaacab999b9",
+            "       00363044aaaaa9accadaab9b99",
+            "      030436302aaaaa99aaadc9999b9",
+            "      06004360000aabba999ad999b9 ",
+            "       63030035560aabba999aa999  ",
+            "       063036633ccaaabba         ",
+            "        06366363c7caaaabaa9c     ",
+            "        003636631c7caaaaaa9dc    ",
+            "         036367771cc9aaa999d7c   ",
+            "        9030adc7771c 9ccddcd7c   ",
+            "      99a2000adc7771  aaccad7c   ",
+            "      c7a22aaaadc7771 99aaadca   ",
+            "      9c77acca aa1777199999ca    ",
+            "     99aca77da    177719999a     ",
+            "     9a9ad77da     177719999     ",
+            "    9aaa9cdda       177719999    ",
+            "    9aaa9cca         177719aaa9  ",
+            "   9caaa9aa         9917771aa249 ",
+            "   9caaaa99         9991771aaaaad",
+            "  9caaaaa9        8899991171caad9",
+            " 9acaaaaa9   8888888899991171cca9",
+            " 9acccca988888888888888889111199 ",
+            "99dc24cc98888888888888888881111  ",
+            "9adccccd9888888888888        11  ",
+            "9acdddd9988888888             11 ",
+            "99aaaaaa888888                  1",
+            "888888888888                     ",
+            " 88888888                        ",
+        ]
+    end
+
+    def initialize
+        super
+        @name = "mmbn-megaman.EXE-colonel-soul"
+        map_color("0", "color_052")
+        map_color("1", "color_083")
+        map_color("2", "color_094")
+        map_color("3", "color_124")
+        map_color("4", "color_136")
+        map_color("5", "color_179")
+        map_color("6", "color_202")
+        map_color("7", "color_231")
+        map_color("8", "color_232")
+        map_color("9", "color_235")
+        map_color("a", "color_236")
+        map_color("b", "color_238")
+        map_color("c", "color_239")
+        map_color("d", "color_244")
+    end
+end
---
+++ lib/arTTY/art/generated/mmbn_megaman_exe_fire_soul.rb
@@ -0,0 +1,84 @@
+# encoding: utf-8
+class ArTTY::Art::MmbnMegamanEXEFireSoul < ArTTY::Art
+    def color_map
+        return [
+            "                                      λ",
+            "                6                   ",
+            "                 6                  ",
+            "                 666                ",
+            "                 6666               ",
+            "                 6666666   6        ",
+            "                  66666666  66      ",
+            "                     677766 666     ",
+            "                     67d77667666    ",
+            "                  6 667dd7667766    ",
+            "                   6667ddd7667666   ",
+            "                   6667dddd67d766   ",
+            "                    66777dd77d766   ",
+            "                   9a667dddddd766   ",
+            "                  9bc667ddddd7766   ",
+            "                  9abc977dddd766    ",
+            "              1   9abbcd77dd776     ",
+            "               11 99abbcc9777769    ",
+            "                1119357bbccdd9cb9   ",
+            "                 11354675bbcccba9   ",
+            "                  1344475aabbba9    ",
+            "                   354653d50999     ",
+            "             11111 9333116069       ",
+            "            1144441aa11111665       ",
+            "            1144672aaa1111111 141   ",
+            "            1144442babb11111 14211  ",
+            "            1122442ccba221  1111421 ",
+            "             111121ccca11   1421211 ",
+            "              1141bc57112  31112141 ",
+            "              1141b574312 935111221 ",
+            "              11211544111905361111  ",
+            "              11aa134 111a0c66511   ",
+            "              acdca1   11b00b533    ",
+            "             abd000a1   1ac0009     ",
+            "             9b00dcba11  1abb9      ",
+            "             9a0d57631111           ",
+            "            a19a56373111111         ",
+            "           c1119566222111111        ",
+            "          ca2111352444211111a       ",
+            "         ac1444132444141111ac9      ",
+            "       9ac112421 1442211bcccb9      ",
+            "      9aba11111  1122111abbba9      ",
+            "      9bcba111    1111 999999       ",
+            "      9accdcca          9aaa9       ",
+            "      99abbcb9          9aaaa9      ",
+            "     9aa99999           9aaaaa99    ",
+            "     9aaaa9            99aaaabbb00  ",
+            "    9aaaa9             9aabbbbb0000 ",
+            "    9bbb99         88880aabbbb00000 ",
+            "   9cccba9    88888888880000aa00000 ",
+            "  0bccccc9888888888888888888000000  ",
+            " 0a0000bc988888888888888888888      ",
+            " 00ab000b9888888888888              ",
+            " 00000000088888888                  ",
+            " 80000000888888                     ",
+            " 888888888888                       ",
+            "  88888888                          ",
+            "                                    ",
+        ]
+    end
+
+    def initialize
+        super
+        @name = "mmbn-megaman.EXE-fire-soul"
+        map_color("0", "color_023")
+        map_color("1", "color_052")
+        map_color("2", "color_088")
+        map_color("3", "color_094")
+        map_color("4", "color_125")
+        map_color("5", "color_130")
+        map_color("6", "color_172")
+        map_color("7", "color_220")
+        map_color("8", "color_233")
+        map_color("9", "color_238")
+        map_color("a", "color_241")
+        map_color("b", "color_245")
+        map_color("c", "color_250")
+        map_color("d", "color_254")
+    end
+end
