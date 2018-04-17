60a66e3ce5e66c33962e5edec4e9b433ecd1acc1
Miles Whittaker <mjwhitta@gmail.com>
2018-04-17 16:51:36 -0400

    LeafGreen second gen seems to have some issues

--- lib/arTTY/art/generated/pkmn_leafgreen_ii_191_sunkern.rb
+++ lib/arTTY/art/generated/pkmn_leafgreen_ii_191_sunkern.rb
@@ -3,36 +3,36 @@ class ArTTY::Art::PkmnLeafgreenII191Sunkern < ArTTY::Art
     def color_map
         return [
             "              22211             λ",
-            "           25533333a          ",
-            "          255522333a          ",
-            "          255253332a          ",
-            "         255253332a           ",
-            "         13253332a            ",
-            "    2112 1223321a             ",
+            "           255333339          ",
+            "          2555223339          ",
+            "          2552533329          ",
+            "         2552533329           ",
+            "         132533329            ",
+            "    2112 12233219             ",
             "  2253332122211               ",
-            " 255552212221    4a   dc      ",
-            " 55522332221     46a d4c      ",
-            " 55253332a22ca   d64a44c      ",
-            "25253332a71d86cad6644c4ca     ",
-            "2553332a77748666d666664ca     ",
-            "2333321 77c46666d8888664b     ",
-            "233211 4cc66d688888888664d    ",
-            " 111   48866d8888848488664d   ",
-            "        d886888888444486644c  ",
-            "        4d688888844caa46664a  ",
-            "       466d8888888c90aa66644c ",
-            "       4ccd8488888aaaead6644a ",
-            "          d4448888aaeeadd6444a",
-            "          d4cc68886aaaddddc44a",
-            "           c9aa8888884adddccca",
-            "           caaa8644dddbdddccca",
-            "           caea84886666ddcccca",
-            "            aa48466666664ccca ",
-            "             dbc46ddd6644444a ",
-            "             cdd66dddccc44ca  ",
-            "              bcc44ccccccca   ",
-            "               bcc44ccccaa    ",
-            "                 cbbaaaa      ",
+            " 255552212221    49   cb      ",
+            " 55522332221     469 c4b      ",
+            " 55253332922b9   c64944b      ",
+            "252533329 1c76b9c6644b4b9     ",
+            "25533329   47666c666664b9     ",
+            "2333321   b46666c7777664a     ",
+            "233211 4bb66c677777777664c    ",
+            " 111   47766c7777747477664c   ",
+            "        c776777777444476644b  ",
+            "        4c677777744b99466649  ",
+            "       466c7777777b809966644b ",
+            "       4bbc7477777999d9c66449 ",
+            "          c444777799dd9cc64449",
+            "          c4bb67776999ccccb449",
+            "           b89977777749cccbbb9",
+            "           b9997644cccacccbbb9",
+            "           b9d974776666ccbbbb9",
+            "            9947466666664bbb9 ",
+            "             cab46ccc66444449 ",
+            "             bcc66cccbbb44b9  ",
+            "              abb44bbbbbbb9   ",
+            "               abb44bbbb99    ",
+            "                 baa9999      ",
         ]
     end
 
@@ -46,13 +46,12 @@ class ArTTY::Art::PkmnLeafgreenII191Sunkern < ArTTY::Art
         map_color("4", "color_143")
         map_color("5", "color_155")
         map_color("6", "color_185")
-        map_color("7", "color_188")
-        map_color("8", "color_226")
-        map_color("9", "color_231")
-        map_color("a", "color_233")
-        map_color("b", "color_237")
-        map_color("c", "color_238")
-        map_color("d", "color_239")
-        map_color("e", "color_243")
+        map_color("7", "color_226")
+        map_color("8", "color_231")
+        map_color("9", "color_233")
+        map_color("a", "color_237")
+        map_color("b", "color_238")
+        map_color("c", "color_239")
+        map_color("d", "color_243")
     end
 end
