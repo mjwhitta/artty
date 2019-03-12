586cf76f1347879da26e79971998d95718126de0
Miles Whittaker <mjwhitta@gmail.com>
2019-03-12 12:44:33 -0400

    Updated some broken art

--- lib/arTTY/art/legend_of_zelda/legend_of_zelda_link.rb
+++ lib/arTTY/art/legend_of_zelda/legend_of_zelda_link.rb
@@ -3,56 +3,56 @@ class ArTTY::Art::LegendOfZeldaLink < ArTTY::Art
         super
         @colors = [
             "                    000000                ",
-            "                  FF6ddd6600              ",
+            "                  0G6ddd6600              ",
             "                 0dddd66dddhhh            ",
-            "                FJ6dd6dddEEEAh            ",
-            "               0I666IddEEEAzAAh           ",
-            "            0000666I6EAAzhEzhEK           ",
-            "          00ye0I6692KzAzhEAhhz22          ",
-            "          0ygg06662hzAhKzAhhKhK2          ",
-            "          0ygg106602hh22hh22JKhJ          ",
-            "           0y110660BK2Kh000fJhhJ          ",
-            "            08yn0660p2hhKO00f2hK          ",
-            "            08e1066602KKoE4ofh2           ",
-            "             0810000KAhJoBBBoh2           ",
-            "              00I66KJAKoBBBBoK            ",
-            "              F6ddda2h20osKx0             ",
-            "             066dddfKK6f0po0              ",
-            "            0l666ddadd6fB00               ",
-            "            00m666dJad6LooL0              ",
-            "           0QM0d6JddaddMQL60              ",
-            "          0QEQ0d60ddKad6MI6J0       0     ",
-            "         0QEEQb0d06dd3KdJ6I00      0S0    ",
-            "        0f0EQM00006ddJIdd600      0j10    ",
-            "       0aaa0M0   0KaKIdKKJ0Q0   00j1j50   ",
-            "       GfaaH0    0I6dddd60QEQ0  0Pj1O5F   ",
-            "      0faaJH000  00J6666J0MQ000 0S1j5y0   ",
-            "      0faKI0jj0  0H0000000M0III00TP5yC0   ",
-            "     0faKI0k770  0IKaEJJM000IJK00T558c0   ",
-            "0F0000aIHI07k80 066JIRMMQ0 GIKJo0T5uuc0   ",
-            "0D7k70JaH08700  06dd666dd60 0IK00T58wc0   ",
-            " 00000HJB0e80  000ddd66dddI0 FIJ0T8cuv0   ",
-            "     00o0gye000jNj0006I6dd60  0IJ0Tc5u0   ",
-            "       00igj0NNTTTNNS000qrof0  000Tc880   ",
-            "        0770 00jNTTTNTTT00000    0j8CC0   ",
-            "        07k0  0tjTNNNNNNTTTT0000  0j8y0   ",
-            "        0j7700o0j000TTTTNNNNTTTT000050    ",
-            "         0kk00rB00  0000TTTTNNNNTTTT000   ",
-            "          00FoBBo0      0000STTTNNNTTtj0  ",
-            "            0rBr0         0o000TTTSNNNNj0 ",
-            "           0FoqoF         0f00K0000STtjjj0",
-            "          0aa000           0JKaa0  000000 ",
-            "          0aaaJ0           0KaaK0         ",
-            "          0afaJ0          0Jaaa0          ",
-            "         0fafaJ0          0JaaK0          ",
-            "         0aafa0           0JKKK0          ",
-            "        0affaJ0          0JJJH0           ",
-            "        0KaaKJ0          0HKKK0           ",
-            "       0KJJJH0           0HHHHI0          ",
-            "       0JJJJH0           0HKaJaf0         ",
-            "      0ffaJKH0           0IIHHIIa0        ",
-            "     000000I0            0JFaJaff0        ",
-            "     0aaffaJ0            00 000000        ",
+            "                GK6dd6dddFFFBh            ",
+            "               0J666JddFFFBABBh           ",
+            "            0000666K6FBAAhTAhFK           ",
+            "          00ze0J6622LABAhFBhhA22          ",
+            "          0zgg06662hABhLABhhLhL2          ",
+            "          0zeg10660Lhh22hh22KLhL          ",
+            "           0z110660CL2Lh000fKhhL          ",
+            "            08zo0660q2hhLP00f2hL          ",
+            "            09e1066602LLpF4pfh2           ",
+            "             0810000LBhKpCCCph2           ",
+            "              00J66LKBLpCCCCpL            ",
+            "              G6ddda2h20ptLy0             ",
+            "             066dddfLL6f0qp0              ",
+            "            0n666ddadk6fC00               ",
+            "            00n666dKad6MppM0              ",
+            "           0RN0d6KddaddNRM60              ",
+            "          0RFR0d60ddKad6NJ6K0       0     ",
+            "         0RFFRb0d06dd3LdK6J00      0S0    ",
+            "        0f0FRN00006ddKJdd600      0l10    ",
+            "       0aaa0N0   0KaLJdLLK0R0   00l1l50   ",
+            "       HfaaI0    0J6dddd60RFR0  0Ql1P5G   ",
+            "      0faaKI000  00K6666K0NR000 0S1l5z0   ",
+            "      0faLI0lm0  0J0000000N0JJJ00TP5zD0   ",
+            "     0faLJ0m770  0KLaFKKN000JKL00T558c0   ",
+            "00000GaIIJ07i70 066KKRNbR0 0JKKp0T5vvc0   ",
+            "0E7m70KaI08700  06dd666dd60 0JL00T58xc0   ",
+            " 00000IKC0g80  000ddd66dddJ0 GJK0T8cvw0   ",
+            "     00p0gze000lOl0006K6dd60  0JK0Tc5v0   ",
+            "       00jgl0OOTTTOOS000rspf0  000Tc880   ",
+            "        0770 00lOTTTOTTT00000    0l8DD0   ",
+            "        07j0  0ulTOOOOOOTTTT0000  0l8z0   ",
+            "        0l7700p0l000TTTTOOOOTTTT000050    ",
+            "         0mm00sC00  0000TTTTOOOOTTTT000   ",
+            "          00GpCCp0      0000STTTOOOTTul0  ",
+            "            0rCs0         0p000TTTSOOOOl0 ",
+            "           00psp0         0f00L0000STuQll0",
+            "          0aa000           0KLaa0  000000 ",
+            "          0aaaK0           0LaaL0         ",
+            "          0afaK0          0Kaaa0          ",
+            "         0fafaK0          0KaaL0          ",
+            "         0fafa0           0KLLL0          ",
+            "        0affaK0          0KKKI0           ",
+            "        0LaaLK0          0ILLL0           ",
+            "       0LKKKI0           0IIIIJ0          ",
+            "       0KKKKI0           0JLaKaf0         ",
+            "      0ffaKLI0           0JJIIJJa0        ",
+            "     000000I0            0L0aLaff0        ",
+            "     0aaffaK0            00 000000        ",
             "     0000000                              ",
         ]
         @name = "legend-of-zelda-link"
@@ -65,7 +65,7 @@ class ArTTY::Art::LegendOfZeldaLink < ArTTY::Art
         map_color("6", "color_065")
         map_color("7", "color_067")
         map_color("8", "color_068")
-        map_color("9", "color_094")
+        map_color("9", "color_069")
         map_color("a", "color_095")
         map_color("b", "color_102")
         map_color("c", "color_104")
@@ -74,42 +74,42 @@ class ArTTY::Art::LegendOfZeldaLink < ArTTY::Art
         map_color("f", "color_131")
         map_color("g", "color_136")
         map_color("h", "color_137")
-        map_color("i", "color_141")
-        map_color("j", "color_146")
-        map_color("k", "color_147")
-        map_color("l", "color_149")
-        map_color("m", "color_150")
-        map_color("n", "color_166")
-        map_color("o", "color_173")
-        map_color("p", "color_174")
-        map_color("q", "color_179")
-        map_color("r", "color_180")
-        map_color("s", "color_187")
-        map_color("t", "color_188")
-        map_color("u", "color_196")
-        map_color("v", "color_203")
-        map_color("w", "color_204")
-        map_color("x", "color_209")
-        map_color("y", "color_220")
-        map_color("z", "color_221")
-        map_color("A", "color_222")
-        map_color("B", "color_223")
-        map_color("C", "color_226")
-        map_color("D", "color_228")
-        map_color("E", "color_231")
-        map_color("F", "color_232")
-        map_color("G", "color_233")
-        map_color("H", "color_236")
-        map_color("I", "color_237")
-        map_color("J", "color_238")
-        map_color("K", "color_239")
-        map_color("L", "color_240")
-        map_color("M", "color_245")
-        map_color("N", "color_248")
-        map_color("O", "color_249")
-        map_color("P", "color_250")
-        map_color("Q", "color_251")
-        map_color("R", "color_252")
+        map_color("i", "color_140")
+        map_color("j", "color_141")
+        map_color("k", "color_143")
+        map_color("l", "color_146")
+        map_color("m", "color_147")
+        map_color("n", "color_150")
+        map_color("o", "color_166")
+        map_color("p", "color_173")
+        map_color("q", "color_174")
+        map_color("r", "color_179")
+        map_color("s", "color_180")
+        map_color("t", "color_187")
+        map_color("u", "color_188")
+        map_color("v", "color_196")
+        map_color("w", "color_203")
+        map_color("x", "color_204")
+        map_color("y", "color_209")
+        map_color("z", "color_220")
+        map_color("A", "color_221")
+        map_color("B", "color_222")
+        map_color("C", "color_223")
+        map_color("D", "color_226")
+        map_color("E", "color_228")
+        map_color("F", "color_231")
+        map_color("G", "color_232")
+        map_color("H", "color_233")
+        map_color("I", "color_236")
+        map_color("J", "color_237")
+        map_color("K", "color_238")
+        map_color("L", "color_239")
+        map_color("M", "color_240")
+        map_color("N", "color_245")
+        map_color("O", "color_248")
+        map_color("P", "color_249")
+        map_color("Q", "color_250")
+        map_color("R", "color_251")
         map_color("S", "color_254")
         map_color("T", "color_255")
     end
