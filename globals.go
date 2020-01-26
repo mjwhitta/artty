6968ee3847e5edc421a5125eac0c1fb225278ed6
Miles Whittaker <mj@whitta.dev>
2020-01-25 21:28:10 -0500

    Version bump

--- globals.go
+++ globals.go
@@ -1,11 +1,9 @@
 package artty
 
-import (
-	"gitlab.com/mjwhitta/artty/cache"
-)
+import "gitlab.com/mjwhitta/artty/cache"
 
 // Cache related vars
 var Cache = cache.New(Version)
 
 // Version is the package version
-const Version = "0.9.44"
+const Version = "0.9.45"
